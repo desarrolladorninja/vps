3a8f887dff37c31877f9e5967904a84e07efa6d4
karrikas <karrikas@gmail.com>
2020-06-09 22:51:03 +0200

    phar generator

---
+++ phar.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * source: https://odan.github.io/2017/08/16/create-a-php-phar-file.html
+ */
+// The php.ini setting phar.readonly must be set to 0
+$pharFile = 'build/vps.phar';
+
+// clean up
+if (file_exists($pharFile)) {
+    unlink($pharFile);
+}
+if (file_exists($pharFile . '.gz')) {
+    unlink($pharFile . '.gz');
+}
+
+// create phar
+$p = new Phar($pharFile);
+
+// creating our library using whole directory
+$p->buildFromDirectory('/app/');
+
+// pointing main file which requires all classes
+$p->setDefaultStub('index.php', '/index.php');
+
+// plus - compressing it into gzip
+$p->compress(Phar::GZ);
+
+unlink('build/vps.phar.gz');
+
+echo "$pharFile successfully created";
